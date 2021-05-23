2c3a9e34417431f6ab829c08b9dd9ec608e423ec
unknown <tanawatt2541@gmail.com>
2021-05-24 01:22:27 +0700

    modified: Edited style dashboard.style

--- src/components/dashboard/views/dashboard.styles.ts
+++ src/components/dashboard/views/dashboard.styles.ts
@@ -1,5 +1,19 @@
 import styled from '@emotion/styled'
+import { Button, Dropdown} from 'antd';
 import bg_image from '../../../assets/main_background.jpg'
+import home_image from './assets/home_outline.png'
+import bar_chart from './assets/bar_chart_square.png'
+import chat_image from './assets/chat.png'
+import group_image from './assets/group.png'
+import settings_image from './assets/settings.png'
+
+const menuicons = {
+    home: home_image,
+    bar: bar_chart,
+    chat: chat_image,
+    group: group_image,
+    settings: settings_image
+}
 
 export const BgContainer = styled.div`
     position: absolute;
@@ -32,14 +46,13 @@ export const MenuLeft = styled.div`
     width: 330px;
     height: 100vh;
     //background: rgba(196, 196, 196, 0.5);
-    padding-top: 150px;
+    padding-top: 120px;
 `
 export const MenuDivInner =  styled.div`
     display:flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
-    padding-top: 20px;
     width: 100%;
     height: 100%;
     //background-color: gray;
@@ -60,15 +73,14 @@ export const MenuDivButton =  styled.div`
     }
 `
 
-export const HomeIcon = styled.div`
-    border: 1px solid #555;
+export const MenuIcon = styled.div<{icon_name: 'home'|'chat'|'group'|'bar'|'settings'}>`
+    //border: 1px solid #555;
     width: 35px;
     height: 35px;
-    background-image: url('./assets/home_outline.png');
+    background-image: ${props => `url(${menuicons[props.icon_name]})`};
     background-position: center;
     background-repeat: no-repeat;
     background-size: contain;
-    //z-index: 1002;
 `
 export const MenuButtonText = styled.p`
     width: 50%;
@@ -79,13 +91,110 @@ export const MenuButtonText = styled.p`
     color: #454545;
     //border: 1px solid #555;
 `
-////////////////////////////
+//////////// Mid body /////////////
+
 export const MidBody = styled.div`
+    padding: 120px 20px 20px 20px;
     width: 920px;
     height: 100vh;
     background-color: rgba(97, 208, 255, 0.2);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    font-family: 'Kanit', sans-serif;
+    font-size: 15px;
+    //border: 1px solid black;
+`
+export const MidTopContainer = styled.div`
+    width: 100%;
+    height: 10vh;
+    padding: 20px 20px 20px 20px;
+
+    background-color: pink;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+
+    border: 1px solid black;
+`
+export const MidMainContainer = styled.div`
+    width: 100%;
+    height: 65vh;
+    padding: 20px 20px 20px 20px;
+    
+    //background-color: purple;
+    border: 1px solid black;
+`
+export const TopPicBox = styled.div`
+    width: 70px;
+    height: 70px;
+    //background-color: gray;
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.2);
+
+    border: 1px solid black;
+`
+export const NameText = styled.p`
+    height: 70px;
+    width: 50%;
+    display: flex;
+    align-items: center;
+
+    border: 1px solid black;
+    margin: 1px 0px 0px 10px;
+    padding-left: 10px;
+    font-size: 20px;
 `
 
+export const GoTaskBtn = styled(Button)`
+    margin-top: 20px;
+    width: 400px;
+    height: 50px;
+    background-color: #454545;
+    color: white;
+    box-shadow: none;
+    border-radius: 10px;
+    border: none;
+    &:hover{
+        background-color: gray;
+        color: white;
+        border: none;
+        box-shadow: none;
+    }
+    &:focus{
+        background-color: gray;
+        color: white;
+        border: none;
+        box-shadow: none;
+    }
+`
+/////////   Top DropDown  ////////
+export const SelectDropDown = styled(Dropdown)`
+    width: 200px;
+    box-shadow: none;
+    margin-left: 10px;
+    &:hover{
+        background-color: gray;
+        color: white;
+        border: none;
+        border-color: gray;
+        box-shadow: none;
+    }
+    &:hover{
+        background-color: gray;
+        color: white;
+        border: none;
+        border-color: gray;
+        box-shadow: none;
+    }
+`
+
+
+//////////////////////////////////
 export const NotificationContainer = styled.div`
     display: flex;
     flex-direction: column;
